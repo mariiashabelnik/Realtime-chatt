28dcefeb0dc32f8265936da3e5bf9b65ee3921a3
Mariia Shabelnik <>
2022-06-22 21:30:56 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import socketIOClient from "socket.io-client";
+import { Input, Stack, Button, Container, Box } from "@chakra-ui/react";
 
 const ENDPOINT = "http://localhost:4000";
 
@@ -34,7 +35,32 @@ function App() {
 
   // show login page
   if (loggedIn === false) {
-    return <div>Log In</div>;
+    return (
+      <div>
+        <Container>
+          <Box borderWidth="1px" p={2} m={1} orderRadius="lg" overflow="hidden">
+            <Stack spacing={3}>
+              <Input
+                placeholder="Enter your nickname:"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                size="md"
+              />
+              <Button
+                onClick={() => {
+                  setLoggedIn(true);
+                }}
+                colorScheme="blue"
+              >
+                Login
+              </Button>
+            </Stack>
+          </Box>
+        </Container>
+      </div>
+    );
   }
 
   // method to send message
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { RecoilRoot } from "recoil";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
